0790a99d0508a5183e7084b44bece4f322173d39
EasyChild <easychild@inbox.ru>
2018-06-09 12:10:36 +0700

    version 1.0.4

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ const web = require('./web.js')
 require("dotenv").config();
 
 let bot;
-console.log(process.env.TOKEN)
-console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'production') {
   bot = new TelegramBot(process.env.TOKEN);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
--- src/web.js
+++ src/web.js
@@ -18,6 +18,7 @@ var server = app.listen(process.env.PORT, "0.0.0.0", () => {
 
 module.exports = (bot) => {
   app.post('/' + bot.token, (req, res) => {
+      console.log(1)
     bot.processUpdate(req.body);
     res.sendStatus(200);
   });
